6ee053a2df01ccb0f4209076d861c285820ee388
Semestotell <kozlov.k.9527@gmail.com>
2022-10-23 11:43:20 +0200

    5451

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from 'components/App';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
